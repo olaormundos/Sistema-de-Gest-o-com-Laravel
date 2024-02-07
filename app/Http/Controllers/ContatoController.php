7d49c9b8bb60e5957d4de453a914c200ba0d81b5
olaormundos <vanderleidasilvaormundo@gmail.com>
2024-02-06 21:16:58 -0300

    Criando controladores

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContatoController extends Controller
+{
+    public function contato() {
+        echo "<h1>Contato</h1>";
+    }
+}
